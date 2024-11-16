852d1a7c04a58242e61029214eca70eb60e6386c
Natalia Kubiak <nataliia.kubiak@gmail.com>
2024-11-16 23:33:36 +0100

    add exception handling to GetAllCurrencies

--- src/main/java/servlet/GetAllCurrenciesServlet.java
+++ src/main/java/servlet/GetAllCurrenciesServlet.java
@@ -3,11 +3,11 @@ package servlet;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletConfig;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.Currency;
+import org.springframework.dao.DataAccessException;
 import servise.CurrencyService;
 
 import java.io.IOException;
@@ -20,18 +20,25 @@ public class GetAllCurrenciesServlet extends HttpServlet {
     private static final String APPLICATION_JSON = "application/json";
 
     @Override
-    public void init(ServletConfig config) throws ServletException {
+    public void init(ServletConfig config) {
         currencyService = new CurrencyService();
     }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try {
+            List<Currency> allCurrenciesList = currencyService.getAll();
+            response.setContentType(APPLICATION_JSON);
+            String allCurrenciesJson = listToJson(allCurrenciesList);
 
-        List<Currency> allCurrenciesList = currencyService.getAll();
-        response.setContentType(APPLICATION_JSON);
-        String allCurrenciesJson = listToJson(allCurrenciesList);
+            response.getWriter().println(allCurrenciesJson);
 
-        response.getWriter().println(allCurrenciesJson);
+        } catch (DataAccessException ex) {
+            ex.printStackTrace();
+
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().println("Database is unavailable. Please try again later.");
+        }
     }
 
     private String listToJson(List<Currency> currencyList) {
