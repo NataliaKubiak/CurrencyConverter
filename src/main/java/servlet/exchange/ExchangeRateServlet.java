b8d7668bba569c9bd491b00caa57bc8ed88daaca
Natalia Kubiak <nataliia.kubiak@gmail.com>
2024-11-19 16:57:26 +0100

    add correct HTTP-method routing with service(): GET and PATCH

--- src/main/java/servlet/exchange/ExchangeRateServlet.java
+++ src/main/java/servlet/exchange/ExchangeRateServlet.java
@@ -22,11 +22,23 @@ public class ExchangeRateServlet extends HttpServlet {
 
     private static final String APPLICATION_JSON = "application/json";
     private static final String ENDPOINT_REGEX = "[A-Z]{6}";
+
     private static final String HTTP_PATCH = "PATCH";
+    private static final String HTTP_GET = "GET";
 
     @Override
     public void init(ServletConfig config) throws ServletException {
         exchangeService = new ExchangeService();
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (HTTP_GET.equals(request.getMethod())) {
+            doGet(request, response);
+
+        } else if (HTTP_PATCH.equals(request.getMethod())) {
+             handlePatch(request, response);
+        }
     }
 
     //Успех - 200 +
@@ -58,41 +70,38 @@ public class ExchangeRateServlet extends HttpServlet {
     //Отсутствует нужное поле формы - 400 +
     //Валютная пара отсутствует в базе данных - 404
     //Ошибка (например, база данных недоступна) - 500
-    @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-         if (HTTP_PATCH.equals(request.getMethod())) {
-             String rateCodes = request.getPathInfo().substring(1).toUpperCase(Locale.ROOT);
+    protected void handlePatch(HttpServletRequest request, HttpServletResponse response) {
+        String rateCodes = request.getPathInfo().substring(1).toUpperCase(Locale.ROOT);
 
-             if(!rateCodes.matches(ENDPOINT_REGEX)) {
-                 response.setStatus(HttpServletResponse.SC_BAD_REQUEST); //400
-                 setResponseText(response, "Invalid currency code in URI. Currency code should consist of 6 letters like: USDEUR, EURRUB, etc.");
-                 return;
-             }
+        if(!rateCodes.matches(ENDPOINT_REGEX)) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST); //400
+            setResponseText(response, "Invalid currency code in URI. Currency code should consist of 6 letters like: USDEUR, EURRUB, etc.");
+            return;
+        }
 
-             String requiredContentType = "application/x-www-form-urlencoded";
+        String requiredContentType = "application/x-www-form-urlencoded";
 
-             if (!requiredContentType.equals(request.getContentType()) || request.getContentType() == null) {
-                 response.setStatus(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE); //415
-                 setResponseText(response, "Invalid Content-Type. Expected application/x-www-form-urlencoded.");
-                 return;
-             }
+        if (!requiredContentType.equals(request.getContentType()) || request.getContentType() == null) {
+            response.setStatus(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE); //415
+            setResponseText(response, "Invalid Content-Type. Expected application/x-www-form-urlencoded.");
+            return;
+        }
 
-             String baseCode = rateCodes.substring(0, 3);
-             String targetCode = rateCodes.substring(3);
+        String baseCode = rateCodes.substring(0, 3);
+        String targetCode = rateCodes.substring(3);
 
-             Optional<ExchangeRatePatchDTO> optionalExchangeRatePatchDTO = ExchangeRatePatchMapper.mapRequestToDto(request, baseCode, targetCode);
+        Optional<ExchangeRatePatchDTO> optionalExchangeRatePatchDTO = ExchangeRatePatchMapper.mapRequestToDto(request, baseCode, targetCode);
 
-             if (optionalExchangeRatePatchDTO.isEmpty()) {
-                 response.setStatus(HttpServletResponse.SC_BAD_REQUEST); //400
-                 //TODO падает и выдает этот статус - разобраться что не так
-                 setResponseText(response, "Invalid request");
+        if (optionalExchangeRatePatchDTO.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST); //400
+            setResponseText(response, "Invalid request");
 
-             } else {
-                 ExchangeRate updatedExchangeRate = exchangeService.patchExchangeRate(optionalExchangeRatePatchDTO.get());
-                 String json = objectToJson(updatedExchangeRate);
+        } else {
+            ExchangeRate updatedExchangeRate = exchangeService.patchExchangeRate(optionalExchangeRatePatchDTO.get());
+            String json = objectToJson(updatedExchangeRate);
 
-                 setResponseText(response, json);
-             }
+            response.setContentType(APPLICATION_JSON);
+            setResponseText(response, json);
         }
     }
 
